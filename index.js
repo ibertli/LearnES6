c998a17eca3bc1646e2638d0685b38841d5f3c40
bertlili <840850106@qq.com>
2021-12-28 22:24:50 +0800

    feat:测试解构时自定义属性名#12345

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-let node = {age: 12, name: "HH"},
-age = "111",
-name = "222";
+let node = {age: 12, name: "HH"};
+// age = "111",
+// name = "222";
+let {name: localName, age} = node;
+// 读取名为name的属性，并将其值赋值给localName
 
-function outputInfo(value){
-    console.log(value === node);
-}
-// let {age, name} = node;
-// ({name, age} = node);
-outputInfo({name, age} = node);
-
+// console.log(age);
+console.log(localName);
 console.log(age);
-console.log(name);
+
