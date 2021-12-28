9d6bf3cea09346b516ffc39ee8287d28b022db9b
bertlili <840850106@qq.com>
2021-12-28 22:05:15 +0800

    feat:测试解构yufa#12345

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-let person = {
-    getGreeting(){
-        return "Hello"
-    }
-};
+let node = {age: 12, name: "HH"},
+age = "111",
+name = "222";
 
-let frined = {
-    getGreeting(){
-        return super.getGreeting() + ", Hi"
-    }
-};
+function outputInfo(value){
+    console.log(value === node);
+}
+// let {age, name} = node;
+// ({name, age} = node);
+outputInfo({name, age} = node);
 
-Object.setPrototypeOf(frined, person);
-
-let relative = Object.create(frined);
-
-console.log(person.getGreeting());
-console.log(frined.getGreeting());
-console.log(relative.getGreeting());
-// frined.getGreeting()方法的[[HomeObject]]属性值是friend，friend的原型是person，所以：
-// friend.getGreeting() 等价于 person.GetGreeting.call(this)
+console.log(age);
+console.log(name);
