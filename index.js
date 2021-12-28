eb7878f7e0b70bbb93a2b2ed613df40e47ca188c
bertlili <840850106@qq.com>
2021-12-28 21:48:01 +0800

    feat:添加注释#12345

--- index.js
+++ index.js
@@ -17,3 +17,5 @@ let relative = Object.create(frined);
 console.log(person.getGreeting());
 console.log(frined.getGreeting());
 console.log(relative.getGreeting());
+// frined.getGreeting()方法的[[HomeObject]]属性值是friend，friend的原型是person，所以：
+// friend.getGreeting() 等价于 person.GetGreeting.call(this)
